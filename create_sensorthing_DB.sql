4903f36716d083dcc668739f0cb60dfad20dfeb0
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-10-13 04:27:53 +0200

    initial commit

---
+++ create_sensorthing_DB.sql
@@ -0,0 +1,13 @@
+﻿-- Database: sensorthings
+
+-- DROP DATABASE sensorthingsserver;
+
+CREATE DATABASE api_tester
+  WITH OWNER = sensorapi
+       TEMPLATE = postgis
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
+       CONNECTION LIMIT = -1;
+
