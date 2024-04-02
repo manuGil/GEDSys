dfb19897a90d64a0fb7703bb04a8783faf41d8fc
Manuel Garcia <gilbert.mg22@gmail.com>
2024-04-02 13:47:56 +0200

    Update update_frequency in DataStreamerConfig class

--- formalizer/bin/steam_generator/config.py
+++ formalizer/bin/steam_generator/config.py
@@ -11,14 +11,13 @@ class DataStreamerConfig(object):
     Configuration for the data streamer.
 
     """
+    event_slug: str  # url slug for the geoevent
     current_time: datetime = datetime.now()
     expiration_time: datetime = current_time.replace(day=current_time.day+1)
-    update_frequency: int = 5 # in seconds
-    api_root = "http://localhost:8080/FROST-Server/v1.0"
-
+    update_frequency: int = 5000 # in milliseconds
     geometry: str = None
     buffer_distance: float = 0
-
+    
 
 if __name__ == "__main__":
     config = DataStreamerConfig()
