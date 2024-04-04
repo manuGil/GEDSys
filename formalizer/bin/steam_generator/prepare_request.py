d6345593d77ad678e3f8aa6930d51024c6167ac4
Manuel Garcia <gilbert.mg22@gmail.com>
2024-04-04 12:46:28 +0200

    Add function to check if extent is a valid WKT polygon

--- formalizer/bin/steam_generator/prepare_request.py
+++ formalizer/bin/steam_generator/prepare_request.py
@@ -5,6 +5,26 @@ Filter to select data streams that intersect with a given extent.
 from shapely import Polygon, wkt
 
 
+def is_valid_wkt_polygon(extent: str) -> bool:
+    """
+    Check if the extent is a valid WKT polygon.
+
+    Parameters
+    ----------
+    extent : str
+        Extent as WKT string.
+
+    Returns
+    -------
+    bool
+        True if valid, False otherwise.
+    """
+    try:
+        polygon = wkt.loads(extent)
+        return isinstance(polygon, Polygon)
+    except:
+        raise ValueError("Invalid WKT polygon extent")
+
 def create_buffer(extent:str, buffer:float) -> str:
     """
     Creates a buffered around the extent.
@@ -22,74 +42,75 @@ def create_buffer(extent:str, buffer:float) -> str:
         Buffered extent as WKT string.
     """
 
-    def is_valid_wkt_polygon(extent: str) -> bool:
-        """
-        Check if the extent is a valid WKT polygon.
-
-        Parameters
-        ----------
-        extent : str
-            Extent as WKT string.
-
-        Returns
-        -------
-        bool
-            True if valid, False otherwise.
-        """
-        try:
-            polygon = wkt.loads(extent)
-            return isinstance(polygon, Polygon)
-        except:
-            return False
-
-    if not is_valid_wkt_polygon(extent):
-        raise ValueError("Invalid WKT polygon extent")
+    is_valid_wkt_polygon(extent)
+    
 
     polygon = wkt.loads(extent)
     buffered_poly = polygon.buffer(buffer, join_style="round")
     return buffered_poly.wkt
 
 
-def request_things_by_extent(root_url:str, detection_extent:str, buffer:float=None) -> str:
+
+#     # TODO: find out if sensor thing api can do coordinates transformation 
+#     # R. I could not find evidence that the API can do coordinate transformation.
+
+
+def prepare_request_observedProperty_location(root_url:str, 
+                                              observedProperty:str, 
+                                              detection_extent:str=None, 
+                                              buffer:float=0) -> str:
     """
-    Defines the select by extent filter.
+    Creates a query that returns a list of observations urls of things that intersect an extent, 
+    and have a given observed property.
 
     Parameters
     ----------
     root_url : str
         Root URL to the sensor API.
-    detection_extent : str
-        Extent as WKT string.
+    observedProperty : str
+        Name of Observed property.
+    detection extent : str, optional
+        Location as WKT string.
     buffer : float, optional
         Buffer around the extent.
 
     Returns
     -------
     str
-        URL for the SensorThings API.
+        fully qualified HTTP request for the SensorThings API.
+
+    Examples:
+    --------
+    root_url = "http://localhost:8080/FROST-Server/v1.0"
+    detection_extent = "POLYGONs((3.8 48, 8.9 48.5, 9 54, 9 49.5, 3.8 48)))"
+    observed_property = "Temperature"
+
+    request = prepare_request_observedProperty_location(root_url, observed_property, detection_extent)
+
     """
-    # TODO: find out if sensor thing api can do coordinates transformation 
-    # R. I could not find evidence that the API can do coordinate transformation.
-    # TODO: include a buffer around the detection extent. R. No apply buffer before forming
-    # request, and provide new polygon with buffer to the request.
 
-    if buffer:
+    if detection_extent:
+        is_valid_wkt_polygon(detection_extent)
+
+    if buffer:    
         detection_extent = create_buffer(detection_extent, buffer)
+    
+    http_request = "{}/Datastreams/$ref?$expand=ObservedProperty,Thing/Locations,Observations&$filter=ObservedProperty/name eq '{}' and geo.intersects(Thing/Locations/location,geography'{}')".format(root_url, observedProperty, detection_extent)
 
-    select_by_extent = root_url + "/Datastreams/$ref?$top=100" + \
-                "&$filter=geo.intersects(Things/Locations/location,geography'" + \
-                detection_extent + "')" 
-
-    return select_by_extent
+    return http_request
 
 
 if __name__ == '__main__':
     
     root_url = "http://localhost:8080/FROST-Server/v1.0"
-    detection_extent = "POLYGON((-4 42, -3.8 43.5, 1 44, 1 42.5, -4 42))"
-    # detection_extent = "LINESTRING (30 10, 10 30, 40 40)"
+    detection_extent = "POLYGON((3.8 48, 8.9 48.5, 9 54, 9 49.5, 3.8 48))"
+    observed_property = "Temperature"
+    buffer = 0.5
 
-    print(request_things_by_extent(root_url, detection_extent, buffer=0.1))
+ 
 
-    # buffered_extent = create_buffered(detection_extent, 0.1)
-    # print(buffered_extent)
+    request = prepare_request_observedProperty_location(root_url, observed_property, detection_extent, buffer)
+
+    print(request)
+
+
