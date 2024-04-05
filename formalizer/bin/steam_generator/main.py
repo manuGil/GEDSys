a9a39f875842b082a60f0299f38be7a6fdcfe68b
Manuel Garcia <gilbert.mg22@gmail.com>
2024-04-05 07:31:03 +0200

    implement use of api config from .env

--- formalizer/bin/steam_generator/main.py
+++ formalizer/bin/steam_generator/main.py
@@ -1,27 +1,42 @@
 """
-Responsible for streaming observations from the SensorThing
-API to the EPE.
-
-- Shall apply detection rules related to detection extent (extent and buffer)
-- Shall send the latest observation for all things within an extent to EPE, continously
-- Shall not send the same observation of thing twice while running.
-- Shall create data streamer for each phenomenon declared in a geoevent.
-- Shall read data streamer configuraton from <geoevent-name>.py
+Responsible for handling events and generating data streams
 """
 
-from prepare_request import request_things_by_extent
-import requests
+
+
+from generator import StreamGenerator, Gevent, SensorAPI, EventProcessorAPI
+
+from datetime import datetime
+from dotenv import load_dotenv
+import os
 
 
 def main():
-    
+
+    load_dotenv("formalizer/bin/config.env") 
     # create request to find things within the extent
-    api_root = "http://localhost:8080/FROST-Server/v1.0"
-    geometry = 'POLYGON((-4 42, -3.8 43.5, 1 44, 1 42.5, -4 42))'
-    buffer_distance = 0.5
+
+    expiration = datetime.now().replace(second=datetime.now().second+10)
+    update_frequency = 5
+    detection_extent = "POLYGON((3.8 48, 8.9 48.5, 9 54, 9 49.5, 3.8 48))"
+    event_name = 'hotday'
+
+    gevent = Gevent(name=event_name, 
+                    expiration=expiration, 
+                    phenomena=['Temperature', 'Relative Humidity'], 
+                    update_frequency=update_frequency,
+                    detection_extent=detection_extent,
+                    buffer_distance=0.5
+                    )
     
-    return request_things_by_extent(api_root, geometry, buffer_distance)
+
+    # global settings
+    sensorthing = SensorAPI(root_url=os.getenv("SENSOR_API"))
+    cep = EventProcessorAPI(root_url=os.getenv("SIDDHI_API"))
+
+    stream_generator = StreamGenerator(gevent, sensorthing, cep)
+    stream_generator.generate()
 
 
 if __name__ == "__main__":
-    print(main())
+    main()
