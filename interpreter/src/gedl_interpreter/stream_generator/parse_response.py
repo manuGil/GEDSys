ab2d5434cfb4756ff146dd263f9d8a97f1497985
Manuel Garcia <gilbert.mg22@gmail.com>
2024-05-20 17:50:45 +0200

    Refactor parse_response.py to use more descriptive function name for parsing datastream response

--- interpreter/src/gedl_interpreter/stream_generator/parse_response.py
+++ interpreter/src/gedl_interpreter/stream_generator/parse_response.py
@@ -47,14 +47,14 @@ Parse the response from from SensorThing API and Siddhi EPE.
 }
 
 
-def parse_response_observedProperty_location(api_response: dict, latest:bool = True) -> dict:
+def parse_response_datastream(datastream_response: dict, latest:bool = True) -> list:
     """
     Parse the response from the SensorThing API (URLs to observations) to extract observations.
 
     Parameters
     ----------
     api_response : dict
-        response from the SensorThing API.
+        response from the SensorThing API. A single datasteam with a list of observations.
     latest : bool, optional
         If True, return the latest observation, otherwise return all found observations ordered
         from latest to oldest.
@@ -62,80 +62,27 @@ def parse_response_observedProperty_location(api_response: dict, latest:bool = T
     Returns
     -------
     dict
-        Dictionary with URLs to observations and location of a Thing with a given observed property.
+        List of observations objects (JSON objects).
 
     Examples
     --------
     """
 
     # get the latest observation
-    if api_response['@iot.count'] == 0:
+    if datastream_response['Observations@iot.count'] == 0:
         print("No observations match the creteria of the API request.")
         return None
     
-    if latest:
-        # count = api_response['value'][0]['Observations@iot.count'] # the number of observations in the response
-        latest_observation = api_response['value'][0]['Observations'][-1]['@iot.selfLink']
-     
-        # get the location of the thing
-        latest_location = api_response['value'][0]['Thing']['Locations'][-1]['@iot.selfLink']
+    observations = [] # obsevaton object
+    
+    for obs in datastream_response['Observations']:
+        observations.append(obs)
 
-        return {'observations': [latest_observation], 'location': latest_location}
-    else:
-        # get all observations
-        observations = [obs['@iot.selfLink'] for obs in api_response['value'][0]['Observations']]
-
-        # assums that the location of the thing is the same for all observations
-        location = api_response['value'][0]['Thing']['Locations'][-1]['@iot.selfLink']
-
-        return {'observations': observations, 'location': location}
+    return observations
 
 
 if __name__ == '__main__':
     
-    dict_response = {
-    "@iot.count": 1,
-    "value": [
-        {
-            "@iot.selfLink": "http://localhost:8080/FROST-Server/v1.0/Datastreams(1)",
-            "ObservedProperty": {
-                "@iot.selfLink": "http://localhost:8080/FROST-Server/v1.0/ObservedProperties(1)"
-            },
-            "Thing": {
-                "@iot.selfLink": "http://localhost:8080/FROST-Server/v1.0/Things(1)",
-                "Locations@iot.count": 1,
-                "Locations": [
-                    {
-                        "@iot.selfLink": "http://localhost:8080/FROST-Server/v1.0/Locations(1)"
-                    },
-                    {
-                        "@iot.selfLink": "http://localhost:8080/FROST-Server/v1.0/Locations(2)"
-                    }
-                ]
-            },
-            "Observations@iot.count": 6,
-            "Observations": [
-                {
-                    "@iot.selfLink": "http://localhost:8080/FROST-Server/v1.0/Observations(1)"
-                },
-                {
-                    "@iot.selfLink": "http://localhost:8080/FROST-Server/v1.0/Observations(2)"
-                },
-                {
-                    "@iot.selfLink": "http://localhost:8080/FROST-Server/v1.0/Observations(3)"
-                },
-                {
-                    "@iot.selfLink": "http://localhost:8080/FROST-Server/v1.0/Observations(4)"
-                },
-                {
-                    "@iot.selfLink": "http://localhost:8080/FROST-Server/v1.0/Observations(5)"
-                },
-                {
-                    "@iot.selfLink": "http://localhost:8080/FROST-Server/v1.0/Observations(6)"
-                }
-            ]
-        }
-    ]
-    }
+    dict_response = {"@iot.selfLink":"http://localhost:8080/FROST-Server/v1.1/Datastreams(73)","@iot.id":73,"name":"Temperature Living Room","description":"The temperature in my living room","observationType":"http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement","unitOfMeasurement":{"name":"Centigrade","symbol":"C","definition":"http://www.qudt.org/qudt/owl/1.0.0/unit/Instances.html#DegreeCentigrade"},"observedArea":{"type":"Point","coordinates":[8.4259727,49.015308]},"phenomenonTime":"2019-03-14T10:00:00Z/2019-03-14T10:05:00Z","Observations@iot.count":6,"Observations":[{"@iot.selfLink":"http://localhost:8080/FROST-Server/v1.1/Observations(50353)","@iot.id":50353,"phenomenonTime":"2019-03-14T10:00:00Z","resultTime":None,"result":21.0},{"@iot.selfLink":"http://localhost:8080/FROST-Server/v1.1/Observations(50354)","@iot.id":50354,"phenomenonTime":"2019-03-14T10:01:00Z","resultTime":None,"result":21.1},{"@iot.selfLink":"http://localhost:8080/FROST-Server/v1.1/Observations(50355)","@iot.id":50355,"phenomenonTime":"2019-03-14T10:02:00Z","resultTime":None,"result":19.0},{"@iot.selfLink":"http://localhost:8080/FROST-Server/v1.1/Observations(50356)","@iot.id":50356,"phenomenonTime":"2019-03-14T10:03:00Z","resultTime":None,"result":19.1},{"@iot.selfLink":"http://localhost:8080/FROST-Server/v1.1/Observations(50357)","@iot.id":50357,"phenomenonTime":"2019-03-14T10:04:00Z","resultTime":None,"result":19.2},{"@iot.selfLink":"http://localhost:8080/FROST-Server/v1.1/Observations(50358)","@iot.id":50358,"phenomenonTime":"2019-03-14T10:05:00Z","resultTime":None,"result":20.0}],"ObservedProperty@iot.navigationLink":"http://localhost:8080/FROST-Server/v1.1/Datastreams(73)/ObservedProperty","Sensor@iot.navigationLink":"http://localhost:8080/FROST-Server/v1.1/Datastreams(73)/Sensor","Thing@iot.navigationLink":"http://localhost:8080/FROST-Server/v1.1/Datastreams(73)/Thing","Observations@iot.navigationLink":"http://localhost:8080/FROST-Server/v1.1/Datastreams(73)/Observations"}
 
-    print(parse_response_observedProperty_location(dict_response))
+    print(parse_response_datastream(dict_response))
